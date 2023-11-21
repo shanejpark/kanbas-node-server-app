c79cb09be202466545c514e9ff7d3a1e428a5b18
Shane Park <shanejpark@gmail.com>
2023-11-20 19:01:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
