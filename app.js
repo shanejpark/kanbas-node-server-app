0d01958a58642d329f94f3d3480d8227410c10dd
Shane Park <shanejpark@gmail.com>
2023-12-04 18:47:12 -0500

    created server

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
